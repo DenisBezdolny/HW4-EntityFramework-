c353184e704849b17c6782db2d285d657be94e98
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-04-27 23:32:51 +0300

    Add new property for migration test

--- HW3/Models/Entities/FriendService.cs
+++ HW3/Models/Entities/FriendService.cs
@@ -12,6 +12,7 @@ namespace HW4.Models.Entities
         public int Id { get; set; }
         public string FriendName { get; set; }
         public string Place { get; set; }
+        public int FriendAge { get; set; }
 
         public List<FriendService> GetFriends()
         {
